98bc5ea97fb4945c38898a12389f05236319b9ca
K-MHOON <azmria117@gmail.com>
2022-04-18 20:34:29 +0900

    Initial commit

---
+++ src/main/java/com/example/cloudnativebatch/CloudNativeBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloudnativebatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudNativeBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudNativeBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cloudnativebatch/CloudNativeBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudnativebatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudNativeBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
