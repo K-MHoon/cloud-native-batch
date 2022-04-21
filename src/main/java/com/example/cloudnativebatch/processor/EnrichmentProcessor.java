4ee3a09824ed447a2ab81f9a3cec1c875e10084c
K-MHOON <azmria117@gmail.com>
2022-04-22 01:07:54 +0900

    feat : @CircuitBreaker, @Recover 활용

--- src/main/java/com/example/cloudnativebatch/processor/EnrichmentProcessor.java
+++ src/main/java/com/example/cloudnativebatch/processor/EnrichmentProcessor.java
@@ -5,6 +5,8 @@ import org.springframework.batch.item.ItemProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.retry.annotation.CircuitBreaker;
+import org.springframework.retry.annotation.Recover;
 import org.springframework.web.client.RestTemplate;
 
 public class EnrichmentProcessor implements ItemProcessor<Foo, Foo> {
@@ -12,6 +14,13 @@ public class EnrichmentProcessor implements ItemProcessor<Foo, Foo> {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Recover
+    public Foo fallback(Foo foo) {
+        foo.setMessage("error");
+        return foo;
+    }
+
+    @CircuitBreaker(maxAttempts = 1)
     @Override
     public Foo process(Foo foo) throws Exception {
         ResponseEntity<String> responseEntity = this.restTemplate.exchange(
